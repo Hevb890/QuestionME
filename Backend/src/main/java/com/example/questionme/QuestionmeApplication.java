0ef6109056b20885e75c6c799816d112b2576f22
Hevindu <95610343+Hevb890@users.noreply.github.com>
2026-09-05 15:47:13 +0530

    Initial BoilerPlate

---
+++ Backend/src/main/java/com/example/questionme/QuestionmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionmeApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/questionme/QuestionmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
