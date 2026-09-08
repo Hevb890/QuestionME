6c134f09ea2fceabdecddd5f049d7c013a97e899
Hevindu <95610343+Hevb890@users.noreply.github.com>
2026-09-08 20:16:31 +0530

    Login logic

--- Backend/authorization/src/main/java/com/example/authorization/config/SecurityConfig.java
+++ Backend/authorization/src/main/java/com/example/authorization/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.authorization.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,5 +34,10 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
