b58542553d6dcb73ad6681b672bf7aecfeb64359
Hevindu <95610343+Hevb890@users.noreply.github.com>
2026-09-18 12:48:30 +0530

    Cleaned code 2

--- Frontend/app/register/page.tsx
+++ Frontend/app/register/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from 'next/link'
+
 export default function RegisterPage() {
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
@@ -26,9 +28,15 @@ export default function RegisterPage() {
                         <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                         <input type="password" name="confirm-password" id="confirm-password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••"/>  
                     </div>
-                    <button type="submit" className="w-full text-white bg-primary-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-300 hover:text-black focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
                     <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">
-                        Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</a>
+                        Already have an account? 
+                        <Link 
+                            href="/login" 
+                            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                        >
+                            Sign in
+                        </Link>
                     </p>
                 </form>
             </div>
