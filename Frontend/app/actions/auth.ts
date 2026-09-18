f5f7175c690f80314c90c38d24ed06a8b90e8448
Hevindu <95610343+Hevb890@users.noreply.github.com>
2026-09-18 23:49:23 +0530

    Register function integration

--- Frontend/app/actions/auth.ts
+++ Frontend/app/actions/auth.ts
@@ -27,3 +27,36 @@ export async function handleLogin(prevState: any, formData: FormData) {
 
     redirect('/register') // Need to change this to dashboard or home page after login. For now, redirecting to register page for testing purposes..
 }
+
+export async function handleRegister(prevState: any, formData: FormData) {
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+    const confirmPassword = formData.get('confirm-password') as string
+
+    if (!email || !password || !confirmPassword) {
+        return { error: "All fields are required" }
+    }
+    
+    if (password !== confirmPassword) {
+        return { error: "Passwords do not match" }
+    }
+
+    try {
+        const response = await fetch(`${process.env.SPRING_BOOT_API_URL}/api/v1/auth/register`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+        })
+
+        const data = await response.json()
+
+        if (!response.ok) {
+            return { error: data.message || "Registration failed" }
+        }
+
+    }catch (error) {
+        return { error: "An error occurred during registration" }
+    }
+
+    redirect('/login') // Redirect to login page after successful registration
+}
